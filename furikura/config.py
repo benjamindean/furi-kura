a1389ca6d856790f43f21071a13a381bbe15a8c6
Benjamin Dean <berman.reshetnikov@gmail.com>
2016-05-20 11:17:13 +0300

    Use LOGIN_ADDRESS from Config

--- furikura/config.py
+++ furikura/config.py
@@ -9,7 +9,8 @@ class Config(object):
 
     CONFIG_FILE = os.path.expanduser('~/.config/furikura/furikura')
     CLIENT_ID = 'RQftQ0QKd9ZCDA'
-    REDIRECT_URI = 'http://localhost:65010/reddit_callback'
+    LOGIN_ADDRESS = 'http://localhost:65010'
+    REDIRECT_URI = LOGIN_ADDRESS + '/reddit_callback'
     USER_AGENT = 'FuriKuraForReddit/0.1 by benjaminabel'
 
     def __init__(self):
