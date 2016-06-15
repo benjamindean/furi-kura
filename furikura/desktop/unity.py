236e57ce477b7cf46ca1c75c6e80c7a933c0d5e8
Benjamin Dean <berman.reshetnikov@gmail.com>
2016-06-15 10:25:20 +0300

    "add_quicklist_item" function for Unity module

--- furikura/desktop/unity.py
+++ furikura/desktop/unity.py
@@ -1,10 +1,18 @@
 import gi
 
 gi.require_version('Unity', '7.0')
-from gi.repository import Unity
+from gi.repository import Unity, Dbusmenu
 
+launcher = Unity.LauncherEntry.get_for_desktop_id("furikura.desktop")
 
 def update_counter(count):
-    launcher = Unity.LauncherEntry.get_for_desktop_id("furikura.desktop")
     launcher.set_property("count", count)
     launcher.set_property("count_visible", True)
+
+def add_quicklist_item(item):
+    quick_list = Dbusmenu.Menuitem.new()
+    list_item = Dbusmenu.Menuitem.new()
+    list_item.property_set(Dbusmenu.MENUITEM_PROP_LABEL, item)
+    list_item.property_set_bool(Dbusmenu.MENUITEM_PROP_VISIBLE, True)
+    quick_list.child_append(list_item)
+    launcher.set_property("quicklist", quick_list)
