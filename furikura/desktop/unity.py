f5f728074b257aac371fc59af8de02e440e57819
Benjamin Dean <berman.reshetnikov@gmail.com>
2016-06-15 22:03:49 +0300

    Apply "Urgent" icon animation on new message

--- furikura/desktop/unity.py
+++ furikura/desktop/unity.py
@@ -1,13 +1,20 @@
 import gi
+from threading import Timer
 
 gi.require_version('Unity', '7.0')
 from gi.repository import Unity, Dbusmenu
+
 
 launcher = Unity.LauncherEntry.get_for_desktop_id("furikura.desktop")
 
 def update_counter(count):
     launcher.set_property("count", count)
     launcher.set_property("count_visible", True)
+
+    if count > 0:
+        launcher.set_property("urgent", True)
+        timer = Timer(3, launcher.set_property, ['urgent', False])
+        timer.start()
 
 def add_quicklist_item(item):
     quick_list = Dbusmenu.Menuitem.new()
