c84ec0003b2c4d25880cbfe5bb43f0cc408aef41
Benjamin Dean <berman.reshetnikov@gmail.com>
2016-06-27 21:25:58 +0300

    Simplify SubredditChooser class

--- furikura/subreddit.py
+++ furikura/subreddit.py
@@ -1,38 +1,39 @@
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
-
 from .utils import get_file
 
 
 class SubredditChooser(object):
 
-    def __init__(self, config_storage):
+    def __init__(self, config_storage, builder):
         self.config_storage = config_storage
+        self.builder = builder
         self.config = self.config_storage.config
-        self.builder = Gtk.Builder()
         self.builder.add_from_file(get_file('furikura/ui/subreddit.xml'))
         self.chooser = self.builder.get_object('choose_subreddit')
 
     def show_window(self):
-        self.subreddit_name = self.builder.get_object('subreddit_name')
-        self.posts_type = self.builder.get_object('posts_type')
+        subreddit_name = self.builder.get_object('subreddit_name')
+        posts_type = self.builder.get_object('posts_type')
 
-        self.subreddit_name.set_text(self.config.get('subreddit'))
-        self.posts_type.set_active(self.config.get('posts_type', 1))
+        subreddit_name.set_text(self.config.get('subreddit', 'No Subreddit'))
+        posts_type.set_active(self.config.get('posts_type', 1))
 
-        signals = {
-            'chooser_destroy': self.__chooser_destroy,
-            'chooser_save': self.__chooser_save
-        }
+        save = self.builder.get_object('save')
+        cancel = self.builder.get_object('cancel')
 
-        self.builder.connect_signals(signals)
+        cancel.connect('clicked', self.__chooser_destroy)
+        save.connect('clicked', self.__chooser_save, (subreddit_name, posts_type))
+
         self.chooser.show()
 
     def __chooser_destroy(self, widget):
         self.chooser.destroy()
 
-    def __chooser_save(self, widget):
-        self.config_storage.set_key('subreddit', self.subreddit_name.get_text())
-        self.config_storage.set_key('posts_type', self.posts_type.get_active())
+    def __chooser_save(self, widget, data):
+        name = data[0].get_text()
+        type = data[1].get_active()
+
+        if name != 'No Subreddit':
+            self.config_storage.set_key('subreddit', name)
+            self.config_storage.set_key('posts_type', type)
+
         self.chooser.destroy()
